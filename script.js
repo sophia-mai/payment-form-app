5223009ef2f84b51f72cc5d5f326f7de88083e88
unknown <sophiathanhmai@gmail.com>
2026-05-07 22:28:42 -0400

    payment-02: Add form validation for expiration date and CVV

---
+++ script.js
@@ -0,0 +1,30 @@
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const month = document.getElementById("month").value;
+  const year = document.getElementById("year").value;
+  if (new Date() > new Date(year, month - 1)) {
+    window.alert("Your card is expired");
+    return;
+  }
+
+  const cvv = document.getElementById("cvv").value;
+  if (!/^[0-9]{3,4}$/.test(cvv)) {
+    window.alert("Invalid CVV. It must be 3 or 4 digits!");
+    return;
+  }
+
+  const cnumber = document.getElementById("cnumber").value;
+  if (!isValid(cnumber)) {
+    window.alert("Invalid card number!");
+    return;
+  }
+
+  window.alert("Thanks for the payment!");
+}
+
+function isValid(cnumber) {
+  return true;
+}
